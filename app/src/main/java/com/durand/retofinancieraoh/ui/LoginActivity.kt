c33f3f97b41cb965e3f113722a8b5bebc18001c3
Josué Durand Pebe <josue.durand8@gmail.com>
2024-07-06 01:23:03 -0500

    login with google

--- app/src/main/java/com/durand/retofinancieraoh/ui/LoginActivity.kt
+++ app/src/main/java/com/durand/retofinancieraoh/ui/LoginActivity.kt
@@ -1,21 +1,144 @@
 package com.durand.retofinancieraoh.ui
 
+import android.content.ContentValues.TAG
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.IntentSenderRequest
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.durand.retofinancieraoh.R
+import com.durand.retofinancieraoh.databinding.ActivityLoginBinding
+import com.google.android.gms.auth.api.identity.BeginSignInRequest
+import com.google.android.gms.auth.api.identity.Identity
+import com.google.android.gms.auth.api.identity.SignInClient
+import com.google.android.gms.common.api.ApiException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
+import java.util.concurrent.Executors
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+    private lateinit var auth: FirebaseAuth
+    private var oneTapClient: SignInClient? = null
+    private lateinit var signInRequest: BeginSignInRequest
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_login)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        auth = Firebase.auth
+        oneTapClient = Identity.getSignInClient(this)
+        signInRequest = BeginSignInRequest.builder()
+            .setGoogleIdTokenRequestOptions(
+                BeginSignInRequest.GoogleIdTokenRequestOptions.builder()
+                    .setSupported(true)
+                    // Your server's client ID, not your Android client ID.
+                    .setServerClientId(getString(R.string.default_web_client_id))
+                    // Only show accounts previously used to sign in.
+                    .setFilterByAuthorizedAccounts(false)
+                    .build()
+            )
+            .build()
+        binding.signInGoogle.setOnClickListener {
+            signInGoogle()
+        }
     }
+
+    override fun onStart() {
+        super.onStart()
+        val currentUser = auth.currentUser
+        if (currentUser != null) {
+            showUser()
+        }
+    }
+
+    private fun showUser() {
+        val user = Firebase.auth.currentUser
+        user?.let {
+            val name = it.displayName
+            val email = it.email
+            val photoUrl = it.photoUrl
+            val emailVerified = it.isEmailVerified
+            binding.txtName.text = name
+            binding.txtEmail.text = email
+            if (emailVerified) {
+                binding.txtStatus.text = "Verified Email"
+            }
+            var image: Bitmap? = null
+            val imageURL = photoUrl.toString()
+            val executorService = Executors.newSingleThreadExecutor()
+            executorService.execute {
+                try {
+                    val `in` = java.net.URL(imageURL).openStream()
+                    image = BitmapFactory.decodeStream(`in`)
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                }
+            }
+            runOnUiThread {
+                try {
+                    Thread.sleep(1000)
+                    binding.imageView.setImageBitmap(image)
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                }
+            }
+        }
+    }
+
+    private fun signInGoogle() {
+        CoroutineScope(Dispatchers.Main).launch {
+            signinGoogle()
+        }
+    }
+
+    private suspend fun signinGoogle() {
+        val result = oneTapClient?.beginSignIn(signInRequest)?.await()
+        val intentSenderRequest = IntentSenderRequest.Builder(result!!.pendingIntent).build()
+        activityResultLauncher.launch(intentSenderRequest)
+    }
+
+    private val activityResultLauncher: ActivityResultLauncher<IntentSenderRequest> =
+        registerForActivityResult(
+            ActivityResultContracts.StartIntentSenderForResult()
+        ) { result ->
+            if (result.resultCode == RESULT_OK) {
+                try {
+                    val credential = oneTapClient!!.getSignInCredentialFromIntent(result.data)
+                    val idToken = credential.googleIdToken
+                    if (idToken != null) {
+                        val firebaseCredential = GoogleAuthProvider.getCredential(idToken, null)
+                        auth.signInWithCredential(firebaseCredential).addOnCompleteListener {
+                            if (it.isSuccessful) {
+                                showUser()
+                            } else {
+                                Log.w(TAG, "signInWithCredential:failure", it.exception)
+                            }
+                        }
+                    }
+                } catch (e: ApiException) {
+                    e.printStackTrace()
+                }
+            }
+        }
 }
